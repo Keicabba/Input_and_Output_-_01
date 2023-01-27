a99b3abe7f57d32b1ed97f0414284eb7c7588346
Keicabba <peppino_cbr@hotmail.com>
2023-01-27 01:27:03 +0100

    Initial commit

---
+++ src/it/figuccia/PrintYourName.java
@@ -0,0 +1,9 @@
+package it.figuccia;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        System.out.println("My name is Pincopallino");
+    }
+}
+// Nota per il tutor: Fare questo esercizio al terzo step di esercizi non ha molto senso.
+// Andava messo all'inizio insieme all'"hello world"
